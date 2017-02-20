cb533dd575a4871e3771337e3125a70772b21df0
Katherine Hambley <superdesigngirl@mac.com>
2017-02-20 16:45:54 -0600

    Add startpage.cs page type to project

---
+++ EpiserverSite2/Models/Pages/StartPage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace EpiserverSite2.Models.Pages
+{
+    [ContentType(DisplayName = "StartPage", GUID = "16723ffd-581b-4337-acde-37023a19e0f2", Description = "")]
+    public class StartPage : PageData
+    {
+
+        [CultureSpecific]
+        [Display(
+            Name = "Main body",
+            Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual XhtmlString MainBody { get; set; }
+
+    }
+}
